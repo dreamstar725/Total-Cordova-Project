43541fd625f7a57817818f4567d0bf3625131bd9
dreamstar725 <dreamstar725@gmail.com>
2017-07-03 03:21:02 -0700

    Add files via upload

---
+++ CordovaProject/CordovaAccelometer/platforms/ios/build/emulator/HelloCordova.app/www/js/index.js
@@ -0,0 +1,74 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+document.getElementById("getAcceleration").addEventListener("click", getAcceleration);
+document.getElementById("watchAcceleration").addEventListener("click", watchAcceleration);
+
+function getAcceleration(){
+   navigator.accelerometer.getCurrentAcceleration(accelerometerSuccess, accelerometerError);
+
+   function accelerometerSuccess(acceleration) {
+      alert('Acceleration X: ' + acceleration.x + '\n' +
+         'Acceleration Y: ' + acceleration.y + '\n' +
+         'Acceleration Z: ' + acceleration.z + '\n' +
+         'Timestamp: '      + acceleration.timestamp + '\n');
+   };
+
+   function accelerometerError() {
+      alert('onError!');
+   };
+	
+}
+
+function watchAcceleration(){
+    
+   var accelerometerOptions = {
+      frequency: 3000
+   }
+
+   var watchID = navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, accelerometerOptions);
+
+   function accelerometerSuccess(acceleration) {
+      alert('Acceleration X: ' + acceleration.x + '\n' +
+         'Acceleration Y: ' + acceleration.y + '\n' +
+         'Acceleration Z: ' + acceleration.z + '\n' +
+         'Timestamp: '      + acceleration.timestamp + '\n');
+
+      setTimeout(function() {
+         navigator.accelerometer.clearWatch(watchID);
+      }, 10000);
+
+   };
+
+   function accelerometerError() {
+      alert('onError!');
+   };
+	
+}
+
+app.initialize();
